6c91559b1923f542f0e6718e42bba186dc642497
Eugenia Musat <musat.eugenia@gmail.com>
2021-05-22 13:19:36 +0000

    Add global variable mainTimer

--- assets/js/main.js
+++ assets/js/main.js
@@ -3,6 +3,9 @@ $(document).ready(function(){
     // call function from autocomplete.js for input with id #satellite-id.
     // this will display suggestion when user starts typing in the inputbox
     autocomplete(document.getElementById("satellite-id"), satelliteList);
+
+    mainTimer = null;
+
     noradIds = [25544, 20580, 48274]
     for (let i=0; i<noradIds.length; i++) {
         // Click event listener for ISS, Hubble, tianhe ID #25544, 20580, 48274
